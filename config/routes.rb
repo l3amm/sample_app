9727a4106584ab882b1c87fb0c7d86fe2b4bef21
Scott Woody <swoody92@gmail.com>
2010-10-18 16:11:35 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  #get "pages/home"
 
-  get "pages/contact"
+  #get "pages/contact"
 
-  get "pages/about"
+  #get "pages/about"
+
+  #get "pages/help"
+
+  root :to => 'pages#home'
+  match '/about', :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
